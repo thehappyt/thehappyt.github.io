63647ed8bf6ed31a00ce56c25778b549fb42909a
Adam Thompson <thompson.adams@gmail.com>
2023-04-29 00:36:31 -0700

    Add Ganja.js script.

---
+++ test.js
@@ -0,0 +1,33 @@
+Algebra(3,0,1,()=>{
+  
+  /*
+  // Direct specification of basis blades using e-notation.
+  var xy_bivector = 1e12,
+      pseudoscalar = 1e012;
+  
+  // Operator overloading .. * = geometric product, ^ = wedge, & = vee, << = dot, >>> = sandwich ... 
+  var xy_bivector_from_product = 1e1 * 1e2;
+  
+  // Directly specified point.
+  var some_point = 1e12 + 0.4e01 + 0.5e02;  
+  
+  // Function that returns point.
+  var function_that_returns_point = ()=>some_point + 0.5e01;
+  
+  // Join of point and function .. notice no calling brackets .. 
+  var join_between_point_and_function = some_point & function_that_returns_point;
+  
+  // Same line as above.. but as function.. (so will update if the point changes)
+  var function_that_returns_join = ()=>some_point & function_that_returns_point;
+  
+  // Binary operations on arrays also work as expected.
+  var even = [1,2,3,4,5]*2;
+  
+  // Even if those contain multivectors or other arrays : 
+  var funky = [1, 1e01+0.5e02, [3,4]] * 3 + [1,2,3];
+  
+  // All elements and functions can be rendered directly. (again, no calling brackets). 
+  var canvas = this.graph([ some_point, function_that_returns_point, function_that_returns_join ]);
+  */
+
+});
